9b8ea0797d8089f8323a39431e073706013427d4
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-07 22:02:35 +0300

    fix: protect system financial article identity

--- raspechatka/raspechatka_os/doctype/financial_article/financial_article.py
+++ raspechatka/raspechatka_os/doctype/financial_article/financial_article.py
@@ -5,6 +5,11 @@ from frappe.utils.nestedset import NestedSet
 
 class FinancialArticle(NestedSet):
 	def validate(self):
+		previous = self.get_doc_before_save()
+		if previous and previous.system_article:
+			protected = ("article_name", "article_type", "active", "system_article")
+			if any(self.get(field) != previous.get(field) for field in protected):
+				frappe.throw(_("Системную финансовую статью нельзя изменять"))
 		if self.parent_financial_article:
 			parent_type = frappe.db.get_value("Financial Article", self.parent_financial_article, "article_type")
 			if parent_type and parent_type != self.article_type:
