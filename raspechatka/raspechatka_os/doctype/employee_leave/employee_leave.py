c79f048a3a9169199dcc18d10aeda92f2516abfe
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-08 19:00:03 +0300

    feat(team): calculate leave duration

--- raspechatka/raspechatka_os/doctype/employee_leave/employee_leave.py
+++ raspechatka/raspechatka_os/doctype/employee_leave/employee_leave.py
@@ -1,7 +1,9 @@
 import frappe
 from frappe.model.document import Document
+from frappe.utils import date_diff, getdate
 
 class EmployeeLeave(Document):
 	def validate(self):
-		if self.date_to < self.date_from:
+		if getdate(self.date_to) < getdate(self.date_from):
 			frappe.throw("Дата окончания не может быть раньше даты начала")
+		self.days = date_diff(self.date_to, self.date_from) + 1
