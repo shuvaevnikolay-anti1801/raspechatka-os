944ad50791496208f6c0d69c168b4aab39b17e62
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-10 18:06:57 +0300

    Mount pilot cashier UX layer

--- pos/src/renderer/src/main.tsx
+++ pos/src/renderer/src/main.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import CashierHotkeys from './CashierHotkeys'
 import DiagnosticsList from './DiagnosticsList'
+import PilotReadiness from './PilotReadiness'
 import PosSafetyPanel from './PosSafetyPanel'
+import ReceiptExplorer from './ReceiptExplorer'
+import SaleSuccessOverlay from './SaleSuccessOverlay'
 import ShiftRecoveryBanner from './ShiftRecoveryBanner'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <CashierHotkeys />
     <ShiftRecoveryBanner />
     <PosSafetyPanel />
+    <PilotReadiness />
+    <ReceiptExplorer />
+    <SaleSuccessOverlay />
     <DiagnosticsList />
+    <div className="cashier-hotkey-help">F2 поиск · F4 оплата · Ctrl+1…6 разделы · Esc закрыть</div>
   </StrictMode>
 )
