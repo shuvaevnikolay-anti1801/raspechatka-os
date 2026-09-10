899fbb10c3bf8ce34b8525a743f419b2ada1a472
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-10 17:45:55 +0300

    Show shift recovery banner globally

--- pos/src/renderer/src/main.tsx
+++ pos/src/renderer/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import DiagnosticsList from './DiagnosticsList'
 import PosSafetyPanel from './PosSafetyPanel'
+import ShiftRecoveryBanner from './ShiftRecoveryBanner'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ShiftRecoveryBanner />
     <PosSafetyPanel />
     <DiagnosticsList />
   </StrictMode>
