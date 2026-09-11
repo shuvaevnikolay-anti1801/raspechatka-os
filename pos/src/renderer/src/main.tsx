84f2228ed74b1e3d03c11c0a5e3bc991d0ad43e7
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-11 10:35:47 +0300

    fix: remove floating cashier overlays

--- pos/src/renderer/src/main.tsx
+++ pos/src/renderer/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import CashierHotkeys from './CashierHotkeys'
-import ReceiptExplorer from './ReceiptExplorer'
 import SaleSuccessOverlay from './SaleSuccessOverlay'
 import SettingsHub from './SettingsHub'
 import ShiftCloseGuard from './ShiftCloseGuard'
@@ -15,8 +14,6 @@ createRoot(document.getElementById('root')!).render(
     <CashierHotkeys />
     <SettingsHub />
     <ShiftCloseGuard />
-    <ReceiptExplorer />
     <SaleSuccessOverlay />
-    <div className="cashier-hotkey-help">F2 поиск · F4 оплата · Ctrl+1…6 разделы · Esc закрыть</div>
   </StrictMode>
 )
