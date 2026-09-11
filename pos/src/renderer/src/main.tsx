b82376d95d019cb421b96e26976438fdae9bdcb4
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-11 14:54:25 +0300

    Включить POS v2 интерфейс

--- pos/src/renderer/src/main.tsx
+++ pos/src/renderer/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './AppV2'
 import CashierHotkeys from './CashierHotkeys'
 import SaleSuccessOverlay from './SaleSuccessOverlay'
 import SettingsHub from './SettingsHub'
 import ShiftCloseGuard from './ShiftCloseGuard'
 import './styles.css'
 import './cashier-cleanup.css'
+import './pos-v2.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
