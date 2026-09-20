baf5f44bcdb5e437a89fb28cb1dcbf9a1f5f10bb
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-20 18:11:27 +0300

    feat: expose access-controlled POS upsell settings API

--- raspechatka/api/sales.py
+++ raspechatka/api/sales.py
@@ -10,6 +10,7 @@ from frappe.utils import cint, flt, get_datetime, get_first_day, now_datetime, n
 from raspechatka.access import get_allowed_entities, get_scope, require_access
 from raspechatka.access_contract import access_contract
 from raspechatka.pos_settings import get_pos_sales_settings
+from raspechatka.pos_upsell import get_pos_upsell_config, save_pos_upsell_rules
 from raspechatka.sales import log_cashier_action, update_shift_totals
 
 
@@ -441,6 +442,23 @@ def save_pos_sales_settings(data):
 			doc.set(fieldname, data[fieldname])
 	doc.save(ignore_permissions=True)
 	return get_pos_sales_settings()
+
+
+@frappe.whitelist()
+@access_contract(area="page.sales.integration", action="read", scope="network")
+def get_pos_upsell_config_api():
+	require_access("page.sales.integration", "read")
+	return get_pos_upsell_config()
+
+
+@frappe.whitelist(methods=["POST"])
+@access_contract(area="page.sales.integration", action="write", scope="network")
+def save_pos_upsell_rules_api(data):
+	require_access("page.sales.integration", "write")
+	if not get_scope()["global"]:
+		frappe.throw(_("Общие настройки продаж доступны только администратору сети"), frappe.PermissionError)
+	data = frappe.parse_json(data) if isinstance(data, str) else data
+	return save_pos_upsell_rules(data)
 
 
 @frappe.whitelist(methods=["POST"])
