4b55c86ed434acf7e28f78705c440ded50bc5945
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-21 07:39:38 +0300

    DEV-159: format receipt status regression test

--- raspechatka/tests/test_pos_warehouse_contract.py
+++ raspechatka/tests/test_pos_warehouse_contract.py
@@ -152,7 +152,7 @@ class TestCanonicalPurchaseOrderReceiptStatus(TestCase):
 				):
 					purchase_order_module.update_received_quantities("PO-1")
 
-				row.db_set.assert_called_once_with("received_quantity", received_quantity, update_modified=False)
+				row.db_set.assert_called_once_with(\n					"received_quantity", received_quantity, update_modified=False\n				)
 				set_value.assert_called_once_with(
 					"Purchase Order",
 					"PO-1",
