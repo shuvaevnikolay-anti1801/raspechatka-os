06485b1fdd362f96097790a2d35731aed85d59df
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-23 01:32:57 +0300

    DEV-169 remove retired legacy stylesheet import

--- pos/src/renderer/src/ShiftCloseGuard.tsx
+++ pos/src/renderer/src/ShiftCloseGuard.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react'
-import './pilot-ux.css'
 import { formatMoney } from './money'
 import { PosButton } from './ui/PosButton'
 import { PosField } from './ui/PosField'
