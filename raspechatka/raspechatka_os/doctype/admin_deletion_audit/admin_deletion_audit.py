ad87a89868531a0d373bdeee425272183a437d22
shuvaevnikolay-anti1801 <shuvaevnikolay@gmail.com>
2026-09-24 18:25:56 +0300

    Protect deletion records against edits and removal

---
+++ raspechatka/raspechatka_os/doctype/admin_deletion_audit/admin_deletion_audit.py
@@ -0,0 +1,11 @@
+import frappe
+from frappe.model.document import Document
+
+
+class AdminDeletionAudit(Document):
+    def validate(self):
+        if not self.is_new():
+            frappe.throw("Immutable record", frappe.PermissionError)
+
+    def on_trash(self):
+        frappe.throw("Immutable record", frappe.PermissionError)
